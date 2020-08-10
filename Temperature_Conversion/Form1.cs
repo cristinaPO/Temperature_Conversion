315dc02990269c9313200c27e5b3ecac137a7fbd
Cristina Punga <cristinik_2004@yahoo.fr>
2020-08-10 23:57:48 +0300

    removed description of conversions

--- Temperature_Conversion/Form1.cs
+++ Temperature_Conversion/Form1.cs
@@ -1,12 +1,4 @@
-﻿/*
-Celsius to Fahrenheit	° F = 9/5 ( ° C) + 32
-Kelvin to Fahrenheit	° F = 9/5 (K - 273) + 32
-Fahrenheit to Celsius	° C = 5/9 (° F - 32)
-Celsius to Kelvin	K = ° C + 273
-Kelvin to Celsius	° C = K - 273
- */
-
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Temperature_Conversion
