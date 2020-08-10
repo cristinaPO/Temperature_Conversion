79d32a5b1363e31092c46f1c2487a32a303a36e5
Cristina Punga <cristinik_2004@yahoo.fr>
2020-08-10 23:45:54 +0300

    Adding unversioned files

---
+++ Temperature_Conversion/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Temperature_Conversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Fahrenheit = new System.Windows.Forms.TextBox();
+            this.txt_Celsius = new System.Windows.Forms.TextBox();
+            this.txt_Kelvin = new System.Windows.Forms.TextBox();
+            this.btn_Fahrenheit = new System.Windows.Forms.Button();
+            this.btn_Celsius = new System.Windows.Forms.Button();
+            this.btn_Kelvin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label1.Location = new System.Drawing.Point(47, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fahrenheit";
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label2.Location = new System.Drawing.Point(83, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Celsius";
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.label3.Location = new System.Drawing.Point(89, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelvin";
+            this.txt_Fahrenheit.Location = new System.Drawing.Point(201, 70);
+            this.txt_Fahrenheit.Name = "txt_Fahrenheit";
+            this.txt_Fahrenheit.Size = new System.Drawing.Size(170, 22);
+            this.txt_Fahrenheit.TabIndex = 3;
+            this.txt_Celsius.Location = new System.Drawing.Point(201, 131);
+            this.txt_Celsius.Name = "txt_Celsius";
+            this.txt_Celsius.Size = new System.Drawing.Size(169, 22);
+            this.txt_Celsius.TabIndex = 4;
+            this.txt_Kelvin.Location = new System.Drawing.Point(201, 185);
+            this.txt_Kelvin.Name = "txt_Kelvin";
+            this.txt_Kelvin.Size = new System.Drawing.Size(170, 22);
+            this.txt_Kelvin.TabIndex = 5;
+            this.btn_Fahrenheit.Location = new System.Drawing.Point(414, 67);
+            this.btn_Fahrenheit.Name = "btn_Fahrenheit";
+            this.btn_Fahrenheit.Size = new System.Drawing.Size(134, 29);
+            this.btn_Fahrenheit.TabIndex = 6;
+            this.btn_Fahrenheit.Text = "Convert";
+            this.btn_Fahrenheit.UseVisualStyleBackColor = true;
+            this.btn_Fahrenheit.Click += new System.EventHandler(this.btn_Fahrenheit_Click);
+            this.btn_Celsius.Location = new System.Drawing.Point(414, 124);
+            this.btn_Celsius.Name = "btn_Celsius";
+            this.btn_Celsius.Size = new System.Drawing.Size(134, 29);
+            this.btn_Celsius.TabIndex = 7;
+            this.btn_Celsius.Text = "Convert";
+            this.btn_Celsius.UseVisualStyleBackColor = true;
+            this.btn_Celsius.Click += new System.EventHandler(this.btn_Celsius_Click);
+            this.btn_Kelvin.Location = new System.Drawing.Point(414, 178);
+            this.btn_Kelvin.Name = "btn_Kelvin";
+            this.btn_Kelvin.Size = new System.Drawing.Size(134, 29);
+            this.btn_Kelvin.TabIndex = 8;
+            this.btn_Kelvin.Text = "Convert";
+            this.btn_Kelvin.UseVisualStyleBackColor = true;
+            this.btn_Kelvin.Click += new System.EventHandler(this.btn_Kelvin_Click);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 460);
+            this.Controls.Add(this.btn_Kelvin);
+            this.Controls.Add(this.btn_Celsius);
+            this.Controls.Add(this.btn_Fahrenheit);
+            this.Controls.Add(this.txt_Kelvin);
+            this.Controls.Add(this.txt_Celsius);
+            this.Controls.Add(this.txt_Fahrenheit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btn_Celsius;
+        private System.Windows.Forms.Button btn_Fahrenheit;
+        private System.Windows.Forms.Button btn_Kelvin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Celsius;
+        private System.Windows.Forms.TextBox txt_Fahrenheit;
+        private System.Windows.Forms.TextBox txt_Kelvin;
+
+        #endregion
+    }
+}
---
+++ Temperature_Conversion/Form1.cs
@@ -0,0 +1,102 @@
+﻿/*
+Celsius to Fahrenheit	° F = 9/5 ( ° C) + 32
+Kelvin to Fahrenheit	° F = 9/5 (K - 273) + 32
+Fahrenheit to Celsius	° C = 5/9 (° F - 32)
+Celsius to Kelvin	K = ° C + 273
+Kelvin to Celsius	° C = K - 273
+ */
+//something extra
+
+using System;
+using System.Windows.Forms;
+
+namespace Temperature_Conversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        private void btn_Fahrenheit_Click(object sender, EventArgs e)
+        {
+            ConvertFromFahrenheit();
+        }
+        private void btn_Celsius_Click(object sender, EventArgs e)
+        {
+            ConvertFromCelsius();
+        }
+        private void btn_Kelvin_Click(object sender, EventArgs e)
+        {
+            ConvertFromKelvin();
+        }
+
+
+        private void ConvertFromFahrenheit()
+        {
+            float f = 0;
+
+            try
+            {
+                f = float.Parse(txt_Fahrenheit.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Try only numbers.");
+            }
+            
+            
+            float c = 5f / 9f * (f - 32);
+            float k = c - 273;
+            
+            txt_Celsius.Text = c.ToString();
+            txt_Kelvin.Text = k.ToString();
+        }
+        private void ConvertFromCelsius()
+        {
+            float c = 0;
+
+            try
+            {
+                c = float.Parse(txt_Celsius.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Try only numbers.");
+            }
+            
+            
+            float f = (c*9) / 5f +32;
+            float k = c + 273;
+            
+            txt_Fahrenheit.Text = f.ToString();
+            txt_Kelvin.Text = k.ToString();
+        }
+        private void ConvertFromKelvin()
+        {
+            float k = 0;
+
+            try
+            {
+                k = float.Parse(txt_Kelvin.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Try only numbers.");
+            }
+            
+            
+            float f = 9f / 5f * (k - 273) + 32;
+            float c = k - 273;
+            
+            txt_Fahrenheit.Text = f.ToString();
+            txt_Celsius.Text = c.ToString();
+        }
+    }
+}
